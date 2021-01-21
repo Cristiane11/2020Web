9b992aced00fb8eabe3bbe83644434f5736f1b8a
Cristiane11 <bjwmcd@yahoo.com>
2021-01-21 05:28:34 -0500

    update

--- web/HTML-Personal Site/nested loops/Loop & Array/index.js
+++ web/HTML-Personal Site/nested loops/Loop & Array/index.js
@@ -5,11 +5,22 @@
 // }
 // ***Loop to array and if statement
 
-const shoesSize=[2,3,6,10,5,13,21,20,24,32,16]
-for (let i=0; i <= shoesSize.length; i++){
-    if (shoesSize[i] % 2 === 0 ){
-        console.log("even number")
-    }else{
-        console.log("Odd Number")
+// const shoesSize=[2,3,6,10,5,13,21,20,24,32,16]
+// for (let i=0; i <= shoesSize.length; i++){
+//     if (shoesSize[i] % 2 === 0 ){
+//         console.log( shoesSize[i] + " " + "even number")
+//     }else{
+//         console.log(shoesSize [i] + " " + "Odd Number")
+//     }
+// }
+
+//*** check it out!
+const people = 56
+
+for (let i =1; i <= people; i++) {
+    if (i % 2 === 0) {
+        console.log('Person ' + i + ' is on the blue team')
+    } else {
+        console.log('Person ' + i + ' is on the red team')
     }
 }
