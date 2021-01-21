5dd15902bfc8b8f609f839e57b1d034e13795b44
Cristiane11 <bjwmcd@yahoo.com>
2021-01-21 04:49:46 -0500

    update

--- web/HTML-Personal Site/nested loops/Loop & Array/index.js
+++ web/HTML-Personal Site/nested loops/Loop & Array/index.js
@@ -1,5 +1,15 @@
 
-const placeVisit = ["Punta Cana","Cancun","Grecia","Algave","California"]
-for (let i=0; i<placeVisit.length; i++){
-    console.log(placeVisit[1])
+// const placeVisit = ["Punta Cana","Cancun","Grecia","Algave","California"]
+// for (let i=0; i<placeVisit.length; i++){
+//     console.log(placeVisit[1])
+// }
+// ***Loop to array and if statement
+
+const shoesSize=[2,3,6,10,5,13,21,20,24,32,16]
+for (let i=0; i <= shoesSize.length; i++){
+    if (shoesSize[i] % 2 === 0 ){
+        console.log("even number")
+    }else{
+        console.log("Odd Number")
+    }
 }
