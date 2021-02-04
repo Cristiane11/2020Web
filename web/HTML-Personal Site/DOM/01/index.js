fcc5d94715f4069fad94bc33589f22c96aef3117
Cristiane11 <bjwmcd@yahoo.com>
2021-02-04 05:38:35 -0500

    update

--- web/HTML-Personal Site/DOM/01/index.js
+++ web/HTML-Personal Site/DOM/01/index.js
@@ -25,3 +25,19 @@ for (let i=0; i<powerRangers.length; i++){
     newName.textContent = powerRangers[i]
 rangersList.append(newName)
 }
+
+//using innerHTML
+var powerRangers = [
+    "Jason Lee Scott", 
+    "Kimberly Hart", 
+    "Zack Taylor", 
+    "Trini Kwan", 
+    "Billy Cranston",
+    "Tommy Oliver"
+]
+
+var rangersList = document.getElementById("rangers")
+
+for (var i = 0; i < powerRangers.length; i++) {
+    rangersList.innerHTML += "<li>" + powerRangers[i] + "</li>"
+}
