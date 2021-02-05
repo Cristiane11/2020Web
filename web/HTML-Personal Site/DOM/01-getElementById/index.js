e2dd73bf01e5c648c1aa34c03af42f9151d1e820
Cristiane11 <bjwmcd@yahoo.com>
2021-02-05 00:03:28 -0500

    update

--- web/HTML-Personal Site/DOM/01-getElementById/index.js
+++ web/HTML-Personal Site/DOM/01-getElementById/index.js
@@ -41,3 +41,18 @@ var rangersList = document.getElementById("rangers")
 for (var i = 0; i < powerRangers.length; i++) {
     rangersList.innerHTML += "<li>" + powerRangers[i] + "</li>"
 }
+// ***** the following object needs to diplay on the code!! It is incomplete
+// var powerRangers = [
+//     {name: "Jason Lee Scott", color: "Red"}, 
+//     {name: "Kimberly Hart", color: "Pink"}, 
+//     {name: "Zack Taylor", color: "Black"}, 
+//     {name: "Trini Kwan", color: "Yellow"}, 
+//     {name: "Billy Cranston", color: "Blue"},
+//     {name: "Tommy Oliver", color: "Green"}
+// ]
+
+// var rangersList = document.getElementById("rangers")
+
+// for (var i = 0; i < powerRangers.length; i++) {
+//     rangersList.innerHTML += "<li>" + powerRangers[i].color + "</li>"
+// }
