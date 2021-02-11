b9d59d7e5fc89e7f42ed9e000e5ddf8dfa198c12
Cristiane11 <bjwmcd@yahoo.com>
2021-02-11 06:34:49 -0500

    update

---
+++ web/HTML-Personal Site/DOM/04-Emoji/emoji01/index.js
@@ -0,0 +1,14 @@
+// Render the emojis as <span> tags in the emojisContainer <div>
+
+// Hints: you can achieve this by creating an span with createElement(),
+// setting its content with textContent, and using append() to append it to the container
+
+const myEmojis = ["Eu", "Tu", "Ele"]
+const emojiContainer = document.getElementById("emojiContainer")
+
+for (let i = 0; i < myEmojis.length; i++) {
+    // write your code here
+    const emoji = document.createElement('span')
+    emoji.textContent = myEmojis[i]
+    emojiContainer.append(emoji)
+}
---
+++ web/HTML-Personal Site/DOM/04-Emoji/playingCode/index.js
@@ -0,0 +1,6 @@
+// 1. Loop through the myEmojis array and log each of them out to the console
+
+const myEmojis = ["EU", "TU ", "Ele"]
+for(let i =0; i< myEmojis.length; i++){
+    console.log(myEmojis[i])
+}
