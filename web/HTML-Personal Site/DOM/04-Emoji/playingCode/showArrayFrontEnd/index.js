dea97ff4a476a122add268bff3deb1ac5cfbf752
Cristiane11 <bjwmcd@yahoo.com>
2021-02-11 06:55:55 -0500

    update

--- web/HTML-Personal Site/DOM/04-Emoji/playingCode/showArrayFrontEnd/index.js
+++ web/HTML-Personal Site/DOM/04-Emoji/playingCode/showArrayFrontEnd/index.js
@@ -1,5 +1,11 @@
 
+// Grab the emojiContainer div and store it in a variable. Log it to the console
+
+
 const myEmojis = ["EU", "TU ", "Ele"]
+let emojiContainer = document.getElementById("emojiContainer")
+console.log(emojiContainerS)
+
 for(let i =0; i< myEmojis.length; i++){
-    console.log(myEmojis[i])
+    //console.log(myEmojis[i])
 }
