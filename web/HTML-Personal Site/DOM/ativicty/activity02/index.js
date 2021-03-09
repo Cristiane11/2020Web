08b6b6b492763d35d99b737d725a00d7736ea6c9
Cristiane11 <bjwmcd@yahoo.com>
2021-03-09 05:57:41 -0500

    update

--- web/HTML-Personal Site/DOM/ativicty/activity02/index.js
+++ web/HTML-Personal Site/DOM/ativicty/activity02/index.js
@@ -6,9 +6,13 @@ const entryTextbox = document.getElementsByClassName('entry-textbox');
 // create a function 
 function addEntryToDom(event){
     event.preventDefault();
+    //Primeiro cria o tag(div)
     const entryDiv = document.createElement('div');
+    // cria a class pra style
     entryDiv.className = 'single-entry';
+    // vc pega o nome da tag criada e assigned com o resultado o que prescisa append 
     entryDiv.innerText = entryTextbox[0].value;
+    //com o nome daonde vai a append poem o nome do que prescisa mostra na frente
     entriesSection.appendChild(entryDiv);
     entryTextbox[0].value = '';
    // console.log(entryTextbox[0].value)
