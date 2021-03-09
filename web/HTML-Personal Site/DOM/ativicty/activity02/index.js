ff7819d555a8b3eb800c3ebe99afc73236d5571a
Cristiane11 <bjwmcd@yahoo.com>
2021-03-08 21:25:20 -0500

    update

--- web/HTML-Personal Site/DOM/ativicty/activity02/index.js
+++ web/HTML-Personal Site/DOM/ativicty/activity02/index.js
@@ -1,9 +1,17 @@
 //entryForm is the id that is wrapping the first section
 const entryForm = document.getElementById('entryForm');
+const entriesSection= document.getElementById('entries');
+const entryTextbox = document.getElementsByClassName('entry-textbox');
 
 // create a function 
 function addEntryToDom(event){
     event.preventDefault();
+    const entryDiv = document.createElement('div');
+    entryDiv.className = 'single-entry';
+    entryDiv.innerText = entryTextbox[0].value;
+    entriesSection.appendChild(entryDiv);
+    entryTextbox[0].value = '';
+   // console.log(entryTextbox[0].value)
 }
 //event listener should be on the wrapping section
 entryForm.addEventListener('submit',addEntryToDom)
