e98d45d3580d8fc6bd1326b3ca0f0583b0f0ddb8
Cristiane11 <bjwmcd@yahoo.com>
2021-03-10 07:11:40 -0500

    update

--- web/HTML-Personal Site/DOM/ativicty/activity02/index.js
+++ web/HTML-Personal Site/DOM/ativicty/activity02/index.js
@@ -1,8 +1,8 @@
 //entryForm is the id that is wrapping the first section
 const entryForm = document.getElementById('entryForm');
-const entriesSection= document.getElementById('entries');
-const entryTextbox = document.getElementsByClassName('entry-textbox');
-
+const entriesSection= document.querySelector('#entries');
+const entryTextbox = document.querySelector('.entry-textbox');
+const entriesNav = document.querySelector('.entries-nav');
 // create a function 
 function addEntryToDom(event){
     event.preventDefault();
@@ -11,11 +11,19 @@ function addEntryToDom(event){
     // cria a class pra style
     entryDiv.className = 'single-entry';
     // vc pega o nome da tag criada e assigned com o resultado o que prescisa append 
-    entryDiv.innerText = entryTextbox[0].value;
+    entryDiv.innerText = entryTextbox.value;
+    //
+    entryDiv.style.display = 'none';
+    console.log(entryDiv);
     //com o nome daonde vai a append poem o nome do que prescisa mostra na frente
     entriesSection.appendChild(entryDiv);
-    entryTextbox[0].value = '';
+    entryTextbox.value = '';
    // console.log(entryTextbox[0].value)
+
+   const displayEntryButton = document.createElement('button');
+   displayEntryButton.className ='display-entry-button';
+   displayEntryButton.innerText =1;
+   entriesNav.appendChild(displayEntryButton);
 }
 //event listener should be on the wrapping section
 entryForm.addEventListener('submit',addEntryToDom)
